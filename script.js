0882d6e1c9d6e7828d7bb3ce447613aba74be4fe
Kweebac <06.amarshall@gmail.com>
2023-02-24 18:04:05 +0000

    Added round function, decides if you won or lost and tells you

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function getComputerChoice() {
 
   switch (randomInt) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
   }
 }
 
@@ -16,5 +16,53 @@ function getPlayerSelection() {
   return prompt("Choose Rock, Paper or Scissors").toLowerCase();
 }
 
+function round(player, computer) {
+  switch (player) {
+    case "rock":
+      switch (computer) {
+        case "rock":
+          console.log("It's a tie! Rock === Rock\nPlayer: Rock\nComputer: Rock");
+          break;
+        case "paper":
+          console.log("You lose! Rock < Paper\nPlayer: Rock\nComputer: Paper");
+          break;
+        case "scissors":
+          console.log("You win! Rock > scissors\nPlayer: Rock\nComputer: Scissors");
+          break;
+      }
+      break;
+    case "paper":
+      switch (computer) {
+        case "rock":
+          console.log("You win! Paper > Rock\nPlayer: Paper\nComputer: Rock");
+          break;
+        case "paper":
+          console.log("It's a tie! Paper === Paper\nPlayer: Paper\nComputer: Paper");
+          break;
+        case "scissors":
+          console.log("You lose! Paper < Scissors\nPlayer: Paper\nComputer: Scissors");
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computer) {
+        case "rock":
+          console.log("You lose! Scissors < Rock\nPlayer: Scissors\nComputer: Rock");
+          break;
+        case "paper":
+          console.log("You win! Scissors > Paper\nPlayer: Scissors\nnComputer: Paper");
+          break;
+        case "scissors":
+          console.log("It's a tie! Scissors === Scissors\nPlayer: Scissors\nnComputer: Scissors");
+          break;
+      }
+      break;
+    default:
+      console.log("Invalid option");
+  }
+}
+
 let computerSelection = getComputerChoice();
 let playerSelection = getPlayerSelection();
+
+round(playerSelection, computerSelection);
